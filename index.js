c10d43a975ee91636a681de80b990edf0aebcb97
Alexander Sierra <alexandercsierra@gmail.com>
2019-11-10 20:06:19 -0500

    completed 6 problems

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function addNumbers(num1, num2) {
   return num1 + num2;
 }
 
+
 // ⭐️ Example Challenge end ⭐️
 
 
@@ -39,9 +40,15 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
+function makePersonObject(idVal, nameVal, emailVal) {
+  return {
+    id: idVal,
+    name: nameVal,
+    email: emailVal
+  };
 }
+
+
 
 /**
  * ### Challenge `getName`
@@ -56,8 +63,8 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(object) {
+  return "Hello, my name is " + object.name;
 }
 
 /**
@@ -73,8 +80,16 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(name) {
+  return {
+    'name': name,
+    sum: function(num1, num2){
+      return num1 + num2;
+    },
+    speak: function(){
+      return "Hello, my name is " + this.name;
+    }
+  }
 }
 
 
